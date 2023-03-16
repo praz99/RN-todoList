a644dbedb59202f949102d9dae29317a8a672593
Praz <t.prazwal@gmail.com>
2023-03-16 07:30:08 -0600

    add to-di-item

---
+++ components/to-do-item/index.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import {Button, StyleSheet, Text, View} from 'react-native';
+
+import {ToDoItem} from '../../models';
+
+interface IProps {
+  todo: ToDoItem;
+  deleteItem: () => void;
+}
+
+const ToDoItemComponent = ({todo: {id, value}, deleteItem}: IProps) => {
+  return (
+    <View style={styles.todoContainer}>
+      <View style={styles.todotTextContainer}>
+        <Text style={styles.sectionTitle}>{value}</Text>
+      </View>
+      <Button
+        onPress={() => deleteItem(id)}
+        title="done"
+        color="#841584"
+        accessibilityLabel="add todo item"
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  todoContainer: {
+    marginTop: 10,
+    paddingHorizontal: 24,
+    backgroundColor: 'deepskyblue',
+    marginLeft: 20,
+    marginRight: 20,
+    borderRadius: 10,
+    borderColor: 'black',
+    borderWidth: 1,
+  },
+  todotTextContainer: {
+    justifyContent: 'center',
+    flexDirection: 'row',
+  },
+  sectionTitle: {
+    fontSize: 20,
+    fontWeight: '400',
+  },
+});
+
+export default ToDoItemComponent;
